ebd020cb15bc704c5a948c88b0497dc92da1157a
keithmccall <keithcodes@gmail.com>
2021-05-18 09:19:11 -0700

    Stg

--- client/src/data/fetch.ts
+++ client/src/data/fetch.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-argument*/
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-argument */
 import { authorization, keychain } from "../keychain";
 
 const options = {
